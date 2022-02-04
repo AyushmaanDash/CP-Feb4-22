987338e9075a1b613fe25d116bad1095fb443b9d
Ayushmaan Dash <97537316+AyushmaanDash@users.noreply.github.com>
2022-02-04 22:02:46 +0530

    Update DragonQuest.cpp

--- DragonQuest.cpp
+++ DragonQuest.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 int main()
 {
-    int t, x, m, n;
+    int t, x, m, n, answer[1000];
     cin >> t;
     for (int i = 0; i < t; i++)
     {
         cin >> x >> n >> m;
         while (x && (m * n))
         {
-            if (((x / 2) + 10) >= x)
+            if ((((x / 2) + 10) >= x) || ((x / 2 - 10) <= 10))
             {
                 x = x - 10;
                 m--;
@@ -23,11 +23,17 @@ int main()
             }
         }
 
-        if (x <= 0)
-            cout << "YES";
+        answer[i] = x;
+    }
+
+    for (int i = 0; i < t; i++)
+    {
+        if (answer[i] <= 0)
+            cout << "YES\n";
         else
-            cout << "NO";
+            cout << "NO\n";
     }
 
     return 0;
 }
+
